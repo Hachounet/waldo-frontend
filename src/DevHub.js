2ab06c87c48f4f58e3e5935dcd2b3ddd8b7b67d5
Hachounet <hachounet.github@gmx.fr>
2024-09-19 15:07:55 +0200

    SWITCH Localhost URL to Prod URL

--- src/DevHub.js
+++ src/DevHub.js
@@ -1,9 +1,11 @@
 // Here are all URL related to backend, switch from localhost to your backend hosting provider.
 
-export const startURL = 'http://localhost:3000/start';
-export const playURL = 'http://localhost:3000/play';
-export const pseudoURL = 'http://localhost:3000/pseudo';
-export const leaderboardURL = 'http://localhost:3000/leaderboard';
+// Backend hosted on https://waldo-backend.adaptable.app
+
+export const startURL = 'https://waldo-backend.adaptable.app/start';
+export const playURL = 'https://waldo-backend.adaptable.app/play';
+export const pseudoURL = 'https://waldo-backend.adaptable.app/pseudo';
+export const leaderboardURL = 'https://waldo-backend.adaptable.app/leaderboard';
 
 // If you want to edit this, edit or change characters in DB
 export const character1 = 'Batman';
