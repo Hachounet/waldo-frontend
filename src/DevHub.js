605a1d35bdb9684b4959e7f3d98e58eb12ec2ee0
Hachounet <hachounet.github@gmx.fr>
2024-09-19 13:35:31 +0200

    FEAT DevHub for maintenability

---
+++ src/DevHub.js
@@ -0,0 +1,6 @@
+// Here are all URL related to backend
+
+export const startURL = 'http://localhost:3000/start';
+export const playURL = 'http://localhost:3000/play';
+export const pseudoURL = 'http://localhost:3000/pseudo';
+export const leaderboardURL = 'http://localhost:3000/leaderboard';
