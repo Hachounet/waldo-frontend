8c4f0d506a2025f429a47eb016628bede210b645
Hachounet <hachounet.github@gmx.fr>
2024-09-19 13:35:58 +0200

    FIX PropTypes Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Image = ({ src, onClick }) => {
   return (
     <img
@@ -10,3 +12,8 @@ const Image = ({ src, onClick }) => {
 };
 
 export default Image;
+
+Image.propTypes = {
+  src: PropTypes.string,
+  onClick: PropTypes.func,
+};
